372c046c69daf643a7e95b0d4414990dd20e805d
Timothy Miller <innerlogic4321@gmail.com>
2023-03-20 21:12:45 +0900

    Update

--- sample/Drastic.FluentUI.Sample.Mac/ViewController.cs
+++ sample/Drastic.FluentUI.Sample.Mac/ViewController.cs
@@ -1,9 +1,14 @@
+using FluentUI.MacOS;
+using Masonry;
 using ObjCRuntime;
 
 namespace Drastic.FluentUI.Sample.Mac;
 
 public partial class ViewController : NSViewController {
-	protected ViewController (NativeHandle handle) : base (handle)
+
+    private MSFButton button;
+
+    protected ViewController (NativeHandle handle) : base (handle)
 	{
 		// This constructor is required if the view controller is loaded from a xib or a storyboard.
 		// Do not put any initialization here, use ViewDidLoad instead.
@@ -13,8 +18,13 @@ public partial class ViewController : NSViewController {
 	{
 		base.ViewDidLoad ();
 
-		// Do any additional setup after loading the view.
-	}
+        this.button = new MSFButton() { Title = "Test Button" };
+
+        this.View!.AddSubview(this.button);
+        this.button.MakeConstraints((make) => {
+            make.Center.EqualTo(this.View!);
+        });
+    }
 
 	public override NSObject RepresentedObject {
 		get => base.RepresentedObject;
